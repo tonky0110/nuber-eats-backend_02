78d71646b5ec48f5aab77f97d888df7a38321437
tonky0110 <tonky0110@gmail.com>
2020-12-09 23:34:03 +0900

    1.1.1 resolver Graphql는 resolver에서 query, mutation을 설정하고, 이를 파일/메모리에서 가지고 있는다. module은 table과 멥핑되는 graphql의 객체.

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,10 @@
+import { Query, Resolver } from '@nestjs/graphql';
+
+@Resolver()
+export class RestaurantResolver{
+    @Query(returns => Boolean)
+    isPizzaGood(): Boolean {
+        return true;
+    }
+}
+ 
