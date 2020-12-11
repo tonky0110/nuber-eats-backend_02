cd1210e64241626561e34ca3a47802c67222e3e1
root <root@DESKTOP-3Q6JVDR.localdomain>
2020-12-11 23:58:12 +0900

    1.3 Query with Args

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver(of => Restaurant)
+@Resolver(of => Restaurant) // Restaurant entity의 resolver를 의미
 export class RestaurantResolver{
     
-    @Query(returns => Restaurant)
-    myRestaurant() {
-        return null;
+    @Query(returns => [Restaurant]) // graphql에서의 return value 설정
+    //            graphql            function 
+    myRestaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] { // typescript에서의 return value설정.
+        console.log(`veganOnly: ${veganOnly}`);
+        return [];
     }
 }
  
