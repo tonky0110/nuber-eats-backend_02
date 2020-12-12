b38f86ab69fef547a5865798c3d175dd09a65baa
tonky0110 <tonky0110@gmail.com>
2020-12-13 01:35:29 +0900

    3.6 type check @Field - for graghql @Column - for typeORM @IsString, IsBoolean, IsNumber, etc. - for Validation check

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,38 +1,39 @@
 import { Field, ObjectType } from "@nestjs/graphql";
-import { IsBoolean, IsString, Length } from "class-validator";
+import { IsBoolean, IsOptional, IsString, Length } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @ObjectType() // 스키마를 빌드하기 위해 사용하는 Graphql decorator
 @Entity() // for TypeORM
 export class Restaurant {
 
-    @Field(type => Number) // Grapqhl entity
-    @PrimaryGeneratedColumn() // typeORM entity
+    @Field(type => Number) // for graphql
+    @PrimaryGeneratedColumn() // for typeORM
     id: number;
 
-    @Field(type => String) // Grapqhl entity
-    @Column() // typeORM entity
-    @IsString() // validation체크
-    @Length(5)
+    @Field(type => String) // for graphql
+    @Column() // for typeORM
+    @IsString() // for validation체크
+    @Length(5) // for validation체크
     name: string;
 
-    @Field(type => Boolean)
-    @Column()
-    @IsBoolean() // validation체크
+    @Field(type => Boolean, { defaultValue: true }) // for graphql
+    @Column({ default: true }) // for typeORM
+    @IsOptional() // for validation체크
+    @IsBoolean() // for validation체크
     isVegan: boolean;
 
-    @Field(type => String)
-    @Column()
-    @IsString() // validation체크
+    @Field(type => String, { defaultValue: '강남'}) // for graphql
+    @Column() // for typeORM
+    @IsString() // for validation체크
     address: string;
 
-    @Field(type => String)
-    @Column()
-    @IsString() // validation체크
+    @Field(type => String) // for graphql
+    @Column() // for typeORM
+    @IsString() // for validation체크
     ownersName: string;
 
-    @Field(type => String)
-    @Column()
-    @IsString() // validation체크
+    @Field(type => String, { defaultValue: '' }) // for graphql
+    @Column({ default: '' }) // for typeORM
+    @IsString() // for validation체크
     cetegoryName: string; 
 }
