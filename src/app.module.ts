d00f76b6e55d4df1caeab6a752f17af2b32256c4
tonky0110 <tonky0110@gmail.com>
2020-12-12 22:36:25 +0900

    2.3 .env파일을 이용한 config설정. @nestjs/config, cross-env설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
-    RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
     TypeOrmModule.forRoot({
       "type": "postgres",
       "host": "localhost",
       "port": 5432,
       "username": "postgres",
+      "password": "tonky0110",
       "database": "nuber-eats",
       "synchronize": true,
       "logging": true,
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
-    })
+    }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
