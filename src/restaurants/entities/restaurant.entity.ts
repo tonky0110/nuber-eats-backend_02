8130faab169ac9e13b0e0c349dbaf71db80c10af
tonky0110 <tonky0110@gmail.com>
2020-12-12 23:53:23 +0900

    3.1 TypeORM Entity 연결

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,15 +1,31 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@ObjectType()
+@ObjectType() // 스키마를 빌드하기 위해 사용하는 Graphql decorator
+@Entity() // for TypeORM
 export class Restaurant {
-    @Field(is => String)
+
+    @Field(type => Number) // Grapqhl entity
+    @PrimaryGeneratedColumn() // typeORM entity
+    id: number;
+
+    @Field(type => String) // Grapqhl entity
+    @Column() // typeORM entity
     name: string;
+
     @Field(type => Boolean)
+    @Column()
     isVegan: boolean;
 
     @Field(type => String)
+    @Column()
     address: string;
 
     @Field(type => String)
+    @Column()
     ownersName: string;
+
+    @Field(type => String)
+    @Column()
+    cetegoryName: string; 
 }
