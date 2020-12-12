6df931ad408ea31659533f2f2043b7ddb3c11c95
tonky0110 <tonky0110@gmail.com>
2020-12-13 00:14:41 +0900

    3.2 Respository를 이용한 데이터 연계. return값이 Promise를 이용하면, return설정도 Promise로 선언해 주어야 한다.

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,15 +1,15 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver(of => Restaurant) // Restaurant entity의 resolver를 의미
 export class RestaurantResolver{
-    
+    constructor(private readonly restaurantService: RestaurantService){};
+
     @Query(returns => [Restaurant]) // graphql에서의 return value 설정
-    //            graphql            function 
-    myRestaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] { // typescript에서의 return value설정.
-        console.log(`veganOnly: ${veganOnly}`);
-        return [];
+    myRestaurants(): Promise<Restaurant[]> { // typescript에서의 return value설정.
+        return this.restaurantService.getAll();
     }
 
     @Mutation(returns => Boolean)
