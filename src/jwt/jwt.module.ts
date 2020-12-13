dee75a6794248bbf2dec5e48a11cacd8ffe22663
tonky0110 <tonky0110@gmail.com>
2020-12-13 23:36:37 +0900

    5.4 Jwt Moudle 2/3 동적 모듈에서 파라메터가 필요한 경우 interface를 생성하고, 이를 전달하여 값을 생성.

--- src/jwt/jwt.module.ts
+++ src/jwt/jwt.module.ts
@@ -1,14 +1,23 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './jwt.interfaces';
 import { JwtService } from './jwt.service';
 
 @Module({})
 @Global()
 export class JwtModule {
-    static forRoot(): DynamicModule {
+    static forRoot(options: JwtModuleOptions): DynamicModule {
         return {
             module: JwtModule,
+            // 동적 생성 시 필요한 option이 필요한 경우.
+            providers: [
+                {
+                    provide: CONFIG_OPTIONS,
+                    useValue: options
+                },
+                JwtService,
+            ],
             exports: [JwtService],
-            providers: [JwtService]
         }
     }
 }
